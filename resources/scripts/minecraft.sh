9dc6644d6f834a7fc6b46ca13d8d167bc3b9980f
Markus Heiliger <markus.heiliger@dash-two.de>
2022-12-30 13:01:16 +0100

    ...

---
+++ resources/scripts/minecraft.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# installing minecraft bedrock server following guidelines
+# documented here https://pimylifeup.com/ubuntu-minecraft-bedrock-server/
+
+# update catalog and upgrade packages
+sudo apt update && sudo apt upgrade -y
+
+# install packages required by minecraft 
+sudo apt install coreutils curl wget unzip grep screen openssl -y
+
+# initialize minecraft variables
+MINECRAFT_USR=$(whoami)
+MINECRAFT_DIR="/minecraft"
+MINECRAFT_LOG="$MINECRAFT_DIR/log"
+
+# make minecraft user a sudoer
+echo "$MINECRAFT_USR  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$MINECRAFT_USR
+
+# initialize minecraft directory
+sudo mkdir $MINECRAFT_DIR && sudo mkdir $MINECRAFT_LOG
+
+# download and unpack latest minecraft version
+DOWNLOAD_URL=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" https://minecraft.net/en-us/download/server/bedrock/ |  grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*')
+sudo wget $DOWNLOAD_URL -O $MINECRAFT_DIR/bedrock-server.zip && sudo unzip $MINECRAFT_DIR/bedrock-server.zip -d $MINECRAFT_DIR/
+
+tee $MINECRAFT_DIR/start.sh <<EOF
+
+sudo screen -L -Logfile $MINECRAFT_LOG/minecraft.\$(date +%Y.%m.%d.%H.%M.%S).log -dmS minecraft /bin/bash -c "LD_LIBRARY_PATH=$MINECRAFT_DIR $MINECRAFT_DIR/bedrock_server"
+
+EOF
+
+tee $MINECRAFT_DIR/stop.sh <<EOF
+
+sudo screen -S minecraft -X quit
+
+EOF
+
+# changing ownership and permissions  
+sudo -n chown -R $MINECRAFT_USR $MINECRAFT_DIR
+sudo -n chmod -R 755 $MINECRAFT_DIR/*.sh
+if [ -e $MINECRAFT_DIR/bedrock_server ]; then
+  sudo -n chmod 755 $MINECRAFT_DIR/bedrock_server
+  sudo -n chmod +x $MINECRAFT_DIR/bedrock_server
+fi
+
+# patching server configuration
+sudo sed -i "/level-seed=/c\level-seed=$PortIPV4" $MINECRAFT_DIR/server.properties
+
+# open minecraft firewall port
+sudo ufw allow 19132 # IPv4
+sudo ufw allow 19133 # IPv6
+
+# write minecraft service configuration
+tee /etc/systemd/system/minecraft.service <<EOF
+
+[Unit]
+Description=Minecraft Bedrock Server
+After=network-online.target
+
+[Service]
+User=$MINECRAFT_USR
+WorkingDirectory=$MINECRAFT_DIR
+Type=forking
+ExecStart=/bin/bash $MINECRAFT_DIR/start.sh
+ExecStop=/bin/bash $MINECRAFT_DIR/stop.sh
+GuessMainPID=no
+TimeoutStartSec=1800
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+# reload service configuration and enable minecraft service
+sudo systemctl daemon-reload \
+  && sudo systemctl enable minecraft.service \
+  && sudo systemctl start minecraft.service
+
+# open dnsmasq firewall port
+# sudo ufw allow dns
